10a017b5c69628ae81f5b949944cdbd93d6767d0
AndreaWolff <awolff@live.ca>
2018-05-20 02:39:46 +1200

    Removed Free banner ad unit id from main app strings resources and moved to Free version only.

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -4,7 +4,9 @@ import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 
-/** An endpoint class we are exposing */
+/**
+ * An endpoint class we are exposing
+ * */
 @Api(
         name = "jokeApi",
         version = "v1",
@@ -17,7 +19,9 @@ import com.google.api.server.spi.config.ApiNamespace;
 
 public class MyEndpoint {
 
-    /** A simple endpoint method that takes a name and says Hi back */
+    /**
+     * A simple endpoint method that returns MyJoke
+     * */
     @ApiMethod(name = "getJoke")
     public MyJoke getJoke() {
         MyJoke response = new MyJoke();
